57e2241d92aa32bc7fa79ccac8a427f633db0703
Xyves <xodrixx@protonmail.com>
2024-12-31 18:10:15 +0100

    Create Routes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,15 @@
 const authRouter = require("express").Router();
-const { authController } = require("../controllers/authController");
-authRouter.post("/login", authController.login);
-authRouter.post("/register", authController.signup);
-authRouter.get("/logout", authController.logout);
-authRouter.get("/:id", authController.getUser);
+const authController = require("../controllers/authController");
+authRouter.post("/auth/login", authController.login);
+authRouter.post(
+  "/auth/register",
+  authController.createUserValidation,
+  authController.validateMiddleware,
+  authController.signup
+);
+authRouter.get("/auth/logout", authController.logout);
+authRouter.get("/auth/:id", authController.getUser);
+userRouter.get("/user/me", authController.getProfile);
+userRouter.get("/user/:id", authController.getUser);
 
 module.exports = authRouter;
--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,14 +1,11 @@
 const chatRouter = require("express").Router();
 const { chatsController } = require("../controllers/chatsController");
-// Get 2 user chat
-chatRouter.get("/", chatsController.getChats);
+// Get chat between 2 users
+chatRouter.get("/", chatsController.getChat);
 
 chatRouter.get("/:chat_id", chatsController.getChatById);
 chatRouter.get("/:chat_id/messages", chatsController.getChatMessages);
-chatRouter.get(
-  "/:chat_id/messages/:message_id",
-  chatsController.getChatMessageById
-);
+chatRouter.get("/messages/:message_id", chatsController.getMessageById);
 
 chatRouter.post("/", chatsController.createChat);
 chatRouter.post("/:chat_id/messages", chatsController.createChatMessage);
