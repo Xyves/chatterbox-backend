e1c98ec944388afa0070d93985149a8dae5e55c0
Xyves <xodrixx@protonmail.com>
2025-02-21 21:34:44 +0100

    Feat: Add token verification for chat routes

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const chatRouter = Router();
 const chatsController = require("../controllers/chatController");
+const { verifyToken } = require("../controllers/authController");
 
-chatRouter.get("/friends", chatsController.getFriends);
-chatRouter.get("/:chat_id", chatsController.getChatWithMessages);
-chatRouter.post("/:chat_id/messages", chatsController.createMessage);
+chatRouter.get("/friends", verifyToken, chatsController.getFriends);
+chatRouter.get("/:chat_id", verifyToken, chatsController.getChatMessages);
+chatRouter.post(
+  "/:chat_id/messages",
+  verifyToken,
+  chatsController.createMessage
+);
 
 module.exports = chatRouter;
